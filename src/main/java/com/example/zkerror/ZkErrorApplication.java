23ef443cfdf7d241d81019761bd06a1e4cd52770
Roman Matiushchenko <robotmrv@gmail.com>
2021-04-11 10:09:02 +0300

    initial commit

---
+++ src/main/java/com/example/zkerror/ZkErrorApplication.java
@@ -0,0 +1,13 @@
+package com.example.zkerror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkErrorApplication.class, args);
+	}
+
+}
